892bdd261896adf90914b757ee88800bd684b509
Tushar Naagar <48862462+TusharNaagar@users.noreply.github.com>
2024-05-11 22:52:06 +0530

    Create Reverse Linklist.py

---
+++ Reverse Linklist.py
@@ -0,0 +1,13 @@
+class Listlist:
+  def __init__(self,value,next):
+    self.value=value
+    self.next=next
+def  reverse(self,listlist):
+  previous=None
+  current=head
+  while  current:
+    temporary=current.next
+    currrent.next=prev
+    previous=current
+    current=temporary
+  return previous
